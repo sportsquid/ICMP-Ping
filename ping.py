a507fc4dc8ebd7d58fe92d4f199bd697f2ef38d6
jtpetkus <jtpetkus@uiowa.edu>
2023-04-21 12:41:16 -0500

    complete

--- ping.py
+++ ping.py
@@ -2,6 +2,7 @@ import socket
 import struct
 import time
 import sys
+import random
 
 if (len(sys.argv) < 2):
     print("An IP adress to ping is required. Use the command 'help' for more info")
@@ -45,63 +46,95 @@ def string_to_bytes(string):
 #different checksum that uses bytearrays instead
 #works properly on my system and is easier to use with the rest of my code
 #due to less conversions
-def checksum(data):
-    if len(data) % 2 != 0:
-        data += b'\x00'
-    sum = 0
-    for i in range(0, len(data), 2):
-        word = (data[i] << 8) + data[i+1]
-        sum += word
-    while (sum >> 16) != 0:
-        sum = (sum & 0xFFFF) + (sum >> 16)
-    return struct.pack('!H', ~sum & 0xFFFF)
+def checksum(string):
+    csum = 0
+    countTo = (len(string) // 2) * 2
+    count = 0
+
+    for count in range(0, countTo, 2):
+        thisVal = string[count+1] * 256 + string[count]
+        csum = csum + thisVal
+        csum = csum & 0xffffffff
+        count = count + 2
+    if countTo < len(string):
+        csum = csum + ord(string[len(string) - 1])
+        csum = csum & 0xffffffff
+    csum = (csum >> 16) + (csum & 0xffff)
+    csum = csum + (csum >> 16)
+    answer = ~csum
+    answer = answer & 0xffff
+    answer = answer >> 8 | (answer << 8 & 0xff00)
+    return answer 
+
+def create_ICMP_packet(packet_type, packet_code, packet_checksum, packet_ID, packet_sequene, packet_data):
+    
+    
+    ICMP_packet = struct.pack("!BBHHH", packet_type, packet_code, packet_checksum, packet_ID, packet_sequene)
+    ICMP_packet = ICMP_packet + packet_data
+    packet_checksum = checksum(ICMP_packet)
+    print("Packet cheskcusm: " + str(packet_checksum))
+    ICMP_packet = struct.pack("!BBHHH", packet_type, packet_code, packet_checksum, packet_ID, packet_sequene) + packet_data
+    
+    return ICMP_packet
+
+
+
+def create_IP_header():
+    ip_header  = b'\x45\x00\x00\x1c'  # Version, IHL, Type of Service | Total Length
+    ip_header += b'\xab\xcd\x00\x00'  # Identification | Flags, Fragment Offset
+    ip_header += b'\x40\x01\xa6\x00'  # TTL, Protocol | Header Checksum
+    ip_header += ip_to_bytes(source_ip) # Source Address
+    ip_header += ip_to_bytes(dest_ip)  # Destination Address
+    return ip_header
 
 source_ip = get_local_ip()
 dest_ip = socket.gethostbyname(sys.argv[1])
 
 
-packet_type = b'\x08'
-packet_code = b'\x00'
-packet_checksum = b'\x00\x00'
-packet_ID = b'\xaa\xaa'
-packet_sequence = b'\xff\xff'
-packet_data = str(time.time()).encode()
-packet = packet_type + packet_code + packet_checksum + packet_ID + packet_sequence + packet_data
-
-print(packet)
-packet_checksum = checksum(packet)
-print(packet_checksum)
-packet = packet_type + packet_code + packet_checksum + packet_ID + packet_sequence +packet_data
 
 
 
 
 
 
-ip_header  = b'\x45\x00\x00\x1c'  # Version, IHL, Type of Service | Total Length
-ip_header += b'\xab\xcd\x00\x00'  # Identification | Flags, Fragment Offset
-ip_header += b'\x40\x01\xa6\x00'  # TTL, Protocol | Header Checksum
-ip_header += ip_to_bytes(source_ip) # Source Address
-ip_header += ip_to_bytes(dest_ip)  # Destination Address
 
 
 
-packet = ip_header + packet
+
+
+
     
-s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW,socket.IPPROTO_ICMP)
+s = socket.socket(socket.AF_INET, socket.SOCK_RAW,socket.IPPROTO_ICMP)
 #s.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
-s.settimeout(1) #waiting a bit longer because program was missing quite a few packets
+s.settimeout(1) 
+sequence_counter = 0
+packet_id = random.randint(0,1000)
 while True:
     print("pinging " + str(dest_ip))
+    timestamp = struct.pack("!d",time.time())
     
-    s.sendto(packet, ("192.168.1.9",0))
+    packet = create_ICMP_packet(8, 0, 0, packet_id, sequence_counter, timestamp) #packet type, packet code, packet checksum, packet id, sequence, payload
+    
+    x = s.sendto(packet, (dest_ip,0))
     
     
     
     try:
-        message = s.recv(100)
-        print("receved packet length" + str(len(message)))
+        message = s.recv(50) #using 50 bytes for buffer because that is full packet size with all internet headers
+        print("receved packet length" + str(len(message[20:])))
+
+        recieved_ICMP_type, recieved_ICMP_code, recieved_ICMP_checksum, recieved_ICMP_id, recieved_ICMP_sequence, recieved_ICMP_payload = struct.unpack("!BBHHHd", message[20:])
+        expected_checksum = checksum(struct.pack("!BBHHHd", recieved_ICMP_type, recieved_ICMP_code, 0, recieved_ICMP_id, recieved_ICMP_sequence, recieved_ICMP_payload))
+        if(recieved_ICMP_type == 0 and recieved_ICMP_code == 0 and recieved_ICMP_checksum == expected_checksum and recieved_ICMP_id == packet_id and recieved_ICMP_sequence == sequence_counter):
+            response_time = round((time.time() - recieved_ICMP_payload) * 1000)
+            print("{} bytes from {} ({}): icmp_seq={} time={} ms".format(len(message), sys.argv[1], dest_ip, recieved_ICMP_sequence, response_time))
+        else:
+            print("invalid packet recieved")
+
+        time.sleep(1) # after successful recieve in time under timeout, wait to prevent packet spam
+        sequence_counter += 1
 
     except socket.timeout:
         print("Packet Lost")
+        sequence_counter += 1
         break
